41bc21e73cd759908b81972f7783c8a943a00f4c
Efnilite <35348263+Efnilite@users.noreply.github.com>
2018-12-26 00:24:25 +0100

    Legacy support

---
+++ master/src/main/java/com/efnilite/skematic/elements/effects/EffRegenerate.java
@@ -0,0 +1,38 @@
+package com.efnilite.skematic.elements.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Name;
+import com.efnilite.skematic.lang.SkematicEffect;
+import com.efnilite.skematic.utils.FaweTools;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+
+@Name("Shape - Line")
+@Description("Create a line between 2 locations")
+public class EffRegenerate extends SkematicEffect {
+
+    static {
+        Skript.registerEffect(EffRegenerate.class, "regenerate [cuboid[(-| )region]] %cuboidregions%");
+    }
+
+    @Override
+    protected void execute(Event e) {
+        CuboidRegion cuboid = (CuboidRegion) expressions[0].getSingle(e);
+
+        if (cuboid == null) {
+            return;
+        }
+
+        EditSession session = FaweTools.getEditSession(Bukkit.getServer().getWorld(cuboid.getWorld().getName()));
+        session.regenerate(cuboid);
+        session.flushQueue();
+    }
+
+    @Override
+    public String toString(Event e, boolean debug) {
+        return "regenerate " + expressions[0].toString(e, debug);
+    }
+}

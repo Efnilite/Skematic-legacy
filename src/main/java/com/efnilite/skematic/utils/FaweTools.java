3be131e2a10284cab4091b607ce51d1778069419
Efnilite <35348263+Efnilite@users.noreply.github.com>
2018-12-31 13:48:11 +0100

    Renamed FaweTools to FaweUtils

--- src/main/java/com/efnilite/skematic/utils/FaweTools.java
+++ src/main/java/com/efnilite/skematic/utils/FaweTools.java
@@ -15,7 +15,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 @SuppressWarnings("deprecation")
-public class FaweUtils {
+public class FaweTools {
 
     public static World getWorld(String s) {
         return FaweAPI.getWorld(s);
